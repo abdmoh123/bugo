4aca4ef6c854c0b2c05e5131da40f56d71982ecf
abdmoh123 <abdhawisa@gmail.com>
2026-05-10 14:28:16 +0100

    feat(CH2/L1): add TextNode and TextType classes
    To represent a block of text.

---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from enum import StrEnum, auto
+from typing import override
+
+
+class TextType(StrEnum):
+    PLAIN = auto()
+    BOLD = auto()
+    ITALIC = auto()
+    CODE = auto()
+    LINK = auto()
+    IMAGE = auto()
+
+
+@dataclass()
+class TextNode:
+    text: str
+    text_type: TextType
+    url: str | None = None
+
+    @override
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
