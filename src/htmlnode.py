48e5765fe2bac39c85f3a5ed6c20db139d285246
abdmoh123 <abdhawisa@gmail.com>
2026-05-12 00:00:08 +0100

    feat(CH2/L4): add LeafNode class
    For tags with no children. Janky implementation imo, but that's what the
    tutorial said.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -44,3 +44,21 @@ class HTMLNode:
     @override
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: HTMLTag | None, value: str, props: dict[str, str] | None = None):
+        super().__init__(tag, value, None, props)
+
+    @override
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    @override
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
