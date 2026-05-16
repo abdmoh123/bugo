2e237354636eb935c42df28e3170c14d29dae1bd
abdmoh123 <abdhawisa@gmail.com>
2026-05-16 01:15:50 +0100

    test(CH4/L1): markdown_to_blocks function
    Normal case, empty input case, and extra new lines case.

--- test/test_md_utils.py
+++ test/test_md_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.md_utils import extract_markdown_images, extract_markdown_links
+from src.md_utils import extract_markdown_images, extract_markdown_links, markdown_to_blocks
 
 
 class TestMDUtils(unittest.TestCase):
@@ -27,6 +27,47 @@ class TestMDUtils(unittest.TestCase):
             "This is text with no links"
         )
         self.assertListEqual([], matches)
+
+
+class TestMarkdownToBlocks(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_markdown_to_blocks_empty(self):
+        md = ""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(blocks, [])
+
+    def test_markdown_to_blocks_extra_newlines(self):
+        md = """
+Test line.
+
+
+Second test line.
+
+
+
+Third test line.
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(blocks, ["Test line.", "Second test line.", "Third test line."])
 
 
 if __name__ == "__main__":
