ece6f72f5e7e4edda7c054c34507b48542aaf81e
abdmoh123 <abdhawisa@gmail.com>
2026-05-16 01:15:19 +0100

    feat(CH4/L1): add markdown to blocks function
    Splits a raw markdown string to multiple blocks of markdown text.

--- src/md_utils.py
+++ src/md_utils.py
@@ -9,3 +9,7 @@ def extract_markdown_images(text: str) -> list[tuple[str, str]]:
 def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     link_regex = r"(?<!!)\[(.*?)\]\((.*?)\)"
     return re.findall(link_regex, text)
+
+
+def markdown_to_blocks(markdown: str) -> list[str]:
+    return [block.strip() for block in markdown.split("\n\n") if block != ""]
