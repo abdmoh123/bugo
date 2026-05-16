991614206fce1ac696deb291cfcc6538ade7d002
abdmoh123 <abdhawisa@gmail.com>
2026-05-16 17:12:13 +0100

    feat(CH4/L2): add md block to block type function
    Also added the BlockType enum.

--- src/markdown.py
+++ src/markdown.py
@@ -1,4 +1,14 @@
+from enum import StrEnum, auto
 import re
+
+
+class BlockType(StrEnum):
+    PARAGRAPH = auto()
+    HEADING = auto()
+    CODE = auto()
+    QUOTE = auto()
+    UNORDERED_LIST = auto()
+    ORDERED_LIST = auto()
 
 
 def extract_markdown_images(text: str) -> list[tuple[str, str]]:
@@ -13,3 +23,23 @@ def extract_markdown_links(text: str) -> list[tuple[str, str]]:
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     return [block.strip() for block in markdown.split("\n\n") if block != ""]
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if block.startswith("#"):
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        return BlockType.QUOTE
+
+    # Check for lists
+    split_block = block.split("\n")
+    if all(line.startswith("- ") for line in split_block):
+        return BlockType.UNORDERED_LIST
+
+    num_regex = r"^\d+\. .*"
+    if all(re.match(num_regex, line)for line in split_block):
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
